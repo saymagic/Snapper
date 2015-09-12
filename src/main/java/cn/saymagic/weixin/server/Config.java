040d3cb6b2a4c61930d30c5a810f39ea567c513f
saymagic <saymagic@163.com>
2015-09-12 20:46:58 +0800

    init

---
+++ src/main/java/cn/saymagic/weixin/server/Config.java
@@ -0,0 +1,11 @@
+package cn.saymagic.weixin.server;
+
+public class Config {
+
+	/** 微信公众号的token，用于验证之用*/
+	public static final String TOKEN = "saymagic";
+	
+	/** 图灵机器人的APPKEY，可在图灵官方申请*/
+	public static final String APIKEY = "44385f3c0d1e1626f05926e1ae990508"; 
+	
+}
